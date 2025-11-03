9f2c6b3ffd2620ac2a767334ee637f5f0c8818c3
sxdrzhou <50129871+sxdrzhou@users.noreply.github.com>
2025-11-03 15:34:59 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // 服务器配置信息
 $config = array(
     'db' => array(
-        'servername' => 'localhost',
+        'servername' => 'dtyz.eicp.net,53043',
         'username' => 'root',
         'password' => '6024402',
         'dbname' => 'sydb',
